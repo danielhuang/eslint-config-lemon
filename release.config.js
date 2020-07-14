424b00070f77fed8b78ec5b71a2eff1f409f773a
Dan <dan@yellowiki.xyz>
2020-07-13 21:28:11 -0400

    fix incorrect formatting

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-  ],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
+	],
 }
